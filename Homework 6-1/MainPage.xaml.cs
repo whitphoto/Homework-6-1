6d4817d417f940347caffb124e77889055cb8dc5
whitphoto <nate@whitchurchphotography.com>
2018-04-11 18:59:56 -0400

    Initial commit

---
+++ Homework 6-1/MainPage.xaml.cs
@@ -0,0 +1,83 @@
+﻿/********************************************************
+*														*
+*		Nathan Whitchurch								*
+*		CIS 223 COMPUTER PROJECTS & APPLICATIONS    	*
+*		Dr Whittle										*
+*		Homework 6-1 Camera App 						*
+*														*
+********************************************************/
+
+
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.Graphics.Imaging;
+using Windows.Media.Capture;
+using Windows.Storage;
+using Windows.Storage.Streams;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Media.Imaging;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace Homework_6_1
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+
+      
+            private async void btnTakePhoto_Click(object sender, RoutedEventArgs e)
+            {
+                CameraCaptureUI captureUI = new CameraCaptureUI();
+                captureUI.PhotoSettings.Format = CameraCaptureUIPhotoFormat.Jpeg;
+                captureUI.PhotoSettings.CroppedSizeInPixels = new Size(200, 200);
+
+                //StorageFile photo  = await StorageFile.GetFileFromApplicationUriAsync(new Uri("ms-appx:///Assets/mypic.jpg"));
+                StorageFile photo = await captureUI.CaptureFileAsync(CameraCaptureUIMode.Photo);
+
+                if (photo == null)
+                {
+                    // User cancelled photo capture
+                    return;
+                }
+
+                StorageFolder destinationFolder = await ApplicationData.Current.LocalFolder.CreateFolderAsync("ProfilePhotoFolder", CreationCollisionOption.OpenIfExists);
+
+                await photo.CopyAsync(destinationFolder, "ProfilePhoto.jpg", NameCollisionOption.ReplaceExisting);
+               // await photo.DeleteAsync();
+
+                IRandomAccessStream stream = await photo.OpenAsync(FileAccessMode.Read);
+                BitmapDecoder decoder = await BitmapDecoder.CreateAsync(stream);
+                SoftwareBitmap softwareBitmap = await decoder.GetSoftwareBitmapAsync();
+
+
+                SoftwareBitmap softwareBitmapBGR8 = SoftwareBitmap.Convert(softwareBitmap, BitmapPixelFormat.Bgra8, BitmapAlphaMode.Premultiplied);
+
+                SoftwareBitmapSource bitmapSource = new SoftwareBitmapSource();
+                await bitmapSource.SetBitmapAsync(softwareBitmapBGR8);
+
+           
+
+            ShowImage.Source = bitmapSource;
+
+        }
+        
+    }
+}
